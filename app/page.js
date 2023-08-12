4b5ad2c6f04e200fbef33626d6d205ff802a0e87
Terry Babyuk <tbabyuk@gmail.com>
2023-08-12 00:56:47 -0400

    animations done, section headers changed, social icons and resume button added

--- app/page.js
+++ app/page.js
@@ -1,16 +1,17 @@
 "use client"
 
-import { useState, useEffect, useRef } from 'react'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image"
+import { Inter } from "next/font/google"
 
-import { skillsArray } from './data/portfolioData'
-import { projectsArray } from './data/portfolioData'
-import { ProjectCard } from './components/ProjectCard'
-import { motion } from 'framer-motion'
+import { skillsArray } from "./data/portfolioData"
+import { projectsArray } from "./data/portfolioData"
+import { ProjectCard } from "./components/ProjectCard"
+import { motion } from "framer-motion"
+import { BsGithub, BsLinkedin } from "react-icons/bs"
+import { BiSolidFilePdf } from "react-icons/bi"
+
 
 import Typewriter from "typewriter-effect"
-
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,56 +26,54 @@ export default function Home() {
   return (
     <main>
       <header className="h-[400px] relative">
-          {/* <div className="social-icons-container">
-              <a href="https://www.linkedin.com/in/taras-terry-babyuk/" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;
-              <a href="https://github.com/tbabyuk" target="_blank"><i className="fa fa-github-square" aria-hidden="true"></i></a>
-            </div> */}
+          <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 1 }}
+              className="flex absolute top-4 right-4">
+              <a href="https://www.linkedin.com/in/taras-terry-babyuk/" target="_blank"><BsLinkedin className="mb-4 text-4xl text-gray-300 opacity-70" /></a>
+              <a href="https://github.com/tbabyuk" target="_blank"><BsGithub className="text-4xl text-gray-300 opacity-70 ms-3" /></a>
+          </motion.div>
+
           <div className="header-content grid md:grid-cols-5 lg:grid-cols-4 w-full h-full bg-[url('/images/header_bg.jpg')] bg-center bg-cover absolute top-[-30px] -skew-y-2 -z-20">
+            <motion.div
+                initial={{ opacity: 0, x: 0, skewY: 2 }}
+                whileInView={{ opacity: 1, x: 0, skewY: 2 }}
+                transition={{ ease: "easeOut", duration: 1 }}
+                className={`grid place-items-center md:justify-end md:col-span-3 lg:col-span-2 skew-y-2`}>
+                <div className="flex flex-col gap-3 mt-10 md:gap-5 lg:gap-8 md:mt-0 text-gray-100">
+                    <h2 className="text-3xl md:text-5xl xl:text-6xl font-light">Hi, I'm Terry</h2>
+                    <h3 className="text-2xl md:text-4xl xl:text-5xl font-semibold">A Front-End Developer</h3>
+                    <h4 className="text-xl md:text-2xl font-thin italic">...with a passion for everything web!</h4>
+                </div>
+            </motion.div>
 
-              <motion.div
-                  initial={{ opacity: 0, x: -200 }}
-                  whileInView={{ opacity: 1, x: 0, skewY: 2 }}
-                  transition={{ ease: "easeOut", duration: 1 }}
-                  className={`grid place-items-center md:justify-end md:col-span-3 lg:col-span-2 skew-y-2`}>
-                  <div className="flex flex-col gap-3 mt-10 md:gap-5 lg:gap-8 md:mt-0 text-gray-100">
-                      <h2 className="text-3xl md:text-5xl xl:text-6xl font-light">Hi, I'm Terry</h2>
-                      <h3 className="text-2xl md:text-4xl xl:text-5xl font-semibold">A Front-End Developer</h3>
-                      <h4 className="text-xl md:text-2xl font-thin italic">...with a passion for everything web!</h4>
-                  </div>
-              </motion.div>
+            <motion.div
+                initial={{ opacity: 0, x: 0, skewY: 2 }}
+                whileInView={{ opacity: 1, x: 0, skewY: 2 }}
+                transition={{ ease: "easeOut", duration: 2 }}
+                className="grid place-items-center md:col-span-2 lg:col-span-2 skew-y-2">
+                <div className="header-right mb-10 md:mb-0 text-2xl md:text-3xl xl:text-4xl text-gray-100">
+                <Typewriter 
+                        options={{
+                        strings: ["HTML/CSS", "Bootstrap", "Tailwind", "JavaScript", "React", "Next.js", "FireBase"],
+                        autoStart: true,
+                        loop: true,
+                        }}
+                    />                
+                </div>
+            </motion.div>
+          </div>
 
-              <motion.div 
-                  initial={{ opacity: 0, x: 200 }}
-                  whileInView={{ opacity: 1, x: 0, skewY: 2 }}
-                  transition={{ ease: "easeOut", duration: 1 }}
-                  className="grid place-items-center md:col-span-2 lg:col-span-2 skew-y-2">
-                  <div className="header-right mb-10 md:mb-0 text-2xl md:text-3xl xl:text-4xl text-gray-100">
-                  <Typewriter 
-                          options={{
-                          strings: ["HTML/CSS", "Bootstrap", "Tailwind", "JavaScript", "React", "NextJS", "FireBase"],
-                          autoStart: true,
-                          loop: true,
-                          }}
-                      />                
-                  </div>
-              </motion.div>
-          </div>  
       </header>
 
 
       {/* ABOUT SECTION */}
-      <section className="about-section py-28 flex flex-col justify-center bg-gray-50" id="about">
-
-        <div className="section-header px-5 lg:px-20 xl:px-48 text-gray-700 pb-20">             
-            <div className="flex flex-col gap-3">
-                <h4 className="text-3xl">ABOUT ME</h4>
-                <p>A bit about who I am and what drives me</p>
-            </div>
-        </div>
-
+      <section className="about-section relative pt-32 pb-44 flex flex-col justify-center bg-gray-50" id="about">
+        <button className="absolute flex items-center top-2 right-8 py-3 px-4 bg-[#f09c6c] hover:bg-[#ec8346] rounded text-gray-50"><BiSolidFilePdf />&nbsp;RESUME</button>
         <div className="flex flex-col lg:flex-row px-5 lg:px-20 xl:px-48">
             <div className="mb-16 lg:mb-0 grid place-items-center lg:me-10 xl:me-20">
-            <img src="/Terry.jpg" style={{maxWidth: "250px"}} className="rounded" alt="photo of Terry" />
+            <img src="/Terry.jpg" alt="photo of Terry" style={{maxWidth: "250px"}} className="rounded"  />
             </div>
             <div className="flex items-center">
                 <div className="leading-8">
@@ -86,21 +85,19 @@ export default function Home() {
 
 
       {/* SKILLS SECTION */}
-      <section className="skills-section py-28 flex flex-col justify-center bg-[url('/images/skills_bg.jpg')]" id="skills">
+      <section className="skills-section relative py-52 flex flex-col justify-center bg-[url('/images/skills_bg.jpg')]">
 
-        <div className="section-header px-5 lg:px-20 xl:px-48 text-gray-50 pb-20">             
-            <div className="flex flex-col gap-3">
-                <h4 className="text-3xl">SKILLS</h4>
-                <p>Some technologies I have worked with</p>
-            </div>
+        <div className="flex flex-col gap-3 bg-sky-800 text-gray-50 text-center py-3 absolute top-[0] -translate-y-[50%] w-full" id="skills">
+            <h4 className="text-3xl">SKILLS</h4>
+            <p className="text-[0.9rem]">Some technologies I have worked with</p>
         </div>
 
         <motion.div
             initial={{ opacity: 0, x: -200 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ ease: "easeOut", duration: 1 }}
-            viewport={{ once: true }}
-            className={`grid grid-cols-4 place-items-center gap-16 px-5 lg:px-8 xl:px-24 skills-list`}>
+            // viewport={{ once: true }}
+            className={`grid grid-cols-2 md:grid-cols-4 place-items-center gap-16 px-5 lg:px-8 xl:px-24 skills-list`}>
             {skillsArray && 
                 skillsArray.map((skill) => (
                 <Image
@@ -119,20 +116,18 @@ export default function Home() {
 
 
       {/* PROJECTS SECTION */}
-      <section className={`projects-section py-28 flex flex-col justify-center bg-gray-50`} id="projects">
+      <section className="projects-section relative py-52 flex flex-col justify-center bg-gray-50">
 
-        <div className="section-header px-5 lg:px-20 xl:px-48 text-gray-700 pb-20">             
-            <div className="flex flex-col gap-3">
-                <h4 className="text-3xl">PROJECTS</h4>
-                <p>A few of my projects</p>
-            </div>
+        <div className="flex flex-col gap-3 bg-sky-800 text-gray-50 text-center py-3 absolute top-[0] -translate-y-[50%] w-full" id="projects">
+            <h4 className="text-3xl">PROJECTS</h4>
+            <p className="text-[0.9rem]">A few of my projects</p>
         </div>
 
         <motion.div
             initial={{ opacity: 0, x: -200 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ ease: "easeOut", duration: 1 }}
-            viewport={{ once: true }}
+            // viewport={{ once: true }}
             className="flex flex-row flex-wrap justify-center gap-20 px-5 lg:px-8 xl:px-18">
             {projectsArray && 
                 projectsArray.map((project) => (
@@ -150,22 +145,20 @@ export default function Home() {
 
 
       {/* CONTACT SECTION */}
-      <section className={`contact-section py-28 flex flex-col justify-center bg-[url('/images/contact_bg.jpg')]`} id="contact">
-    
-        <div className="section-header px-5 lg:px-20 xl:px-48 text-gray-50 pb-20">             
-            <div className="flex flex-col gap-3">
-                <h4 className="text-3xl">CONTACT ME</h4>
-                <p>Let's get in touch!</p>
-            </div>
+      <section className="contact-section relative py-52 flex flex-col justify-center bg-[url('/images/contact_bg.jpg')]">
+
+        <div className="flex flex-col gap-3 bg-sky-800 text-gray-50 text-center py-3 absolute top-[0] -translate-y-[50%] w-full" id="contact">
+            <h4 className="text-3xl">CONTACT ME</h4>
+            <p className="text-[0.9rem]">Let's get in touch!</p>
         </div>
 
         <motion.div
             initial={{ opacity: 0, x: -200 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ ease: "easeOut", duration: 1 }}
-            viewport={{ once: true }}
+            // viewport={{ once: true }}
             className={`flex justify-center contact-form px-5 lg:px-20 xl:px-48`}>
-              <form className="flex flex-col w-[500px] p-5">
+              <form className="flex flex-col w-[500px]">
                   <input type="text" name="name" placeholder="your name" className="py-2 ps-3 rounded mb-7" />
                   <input type="email" name="email" placeholder="your email" className="py-2 ps-3 rounded mb-7" />
                   <textarea name="message" id="" cols="30" rows="5" placeholder="your message" className="py-2 ps-3 rounded mb-12" />
