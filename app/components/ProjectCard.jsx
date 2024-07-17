8cba45e72a64dedeec730a61c8f425d0304a0d7e
Terry Babyuk <tbabyuk@gmail.com>
2024-07-17 18:51:35 -0400

    fixed project card link issue

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -8,18 +8,16 @@ export const ProjectCard = ({project}) => {
 
 
   return (
-    <div className="flex flex-col rounded-md overflow-hidden w-auto md:w-[330px] shadow-md bg-gray-50 card-box">
-        <Link href={`/project/${url}#project-title`}>
+    <Link href={`/project/${url}#project-title`} className="flex flex-col rounded-md overflow-hidden w-auto md:w-[330px] shadow-md bg-gray-50 card-box">
             <div className="overflow-hidden">
                 <img src={src} alt={alt} />
             </div>
-        </Link>
         <div className="">
             <div className="text-center py-6 border-t-2 border-gray-200 text-gray-800">
                 <h2 className="text-2xl font-semibold mb-3">{title}</h2>
                 <p className="text-sm">{shortDescription}</p>
             </div>
         </div>
-    </div>  
+    </Link>  
   )
 }
