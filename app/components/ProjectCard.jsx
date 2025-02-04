ee766706702c262a1970d349db6976b5e80587a2
Terry Babyuk <tbabyuk@gmail.com>
2025-02-03 20:52:05 -0500

    finalized text content for music school, realtor and spa website projects

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -13,7 +13,7 @@ export const ProjectCard = ({project}) => {
                 <img src={src} alt={alt} />
             </div>
         <div className="">
-            <div className="text-center py-6 border-t-2 border-gray-200 text-gray-800">
+            <div className="text-center py-4 border-t-2 border-gray-200 text-gray-800">
                 <h2 className="text-[22px] font-semibold mb-3">{title}</h2>
                 <p className="text-sm">{shortDescription}</p>
             </div>
